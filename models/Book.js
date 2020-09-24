235a795097d05e850df3fd9aabe28477de04ce74
Diego Pineda <pineda.diego798@gmail.com>
2020-09-24 14:51:39 -0500

    Set book pdf url function

--- models/Book.js
+++ models/Book.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const { appConfig } = require('../config')
 
 const Schema = mongoose.Schema
 const BookSchema = Schema({
@@ -10,5 +11,13 @@ const BookSchema = Schema({
 }, {
   timestamps: true
 })
+
+UserSchema.methods.setPdfUrl = function (filename) {
+  const { host, port } = appConfig
+  if (process.env.NODE_ENV === 'production')
+    this.pdfUrl = `${host}/docs/${filename}`
+  else
+    this.pdfUrl = `${host}:${port}/docs/${filename}`
+}
 
 module.exports = mongoose.model('Books', BookSchema)
