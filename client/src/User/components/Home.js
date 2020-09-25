0ba23018282e3d52d9da2eb260b340c8113960b2
Diego Pineda <pineda.diego798@gmail.com>
2020-09-25 13:59:13 -0500

    Home user component

---
+++ client/src/User/components/Home.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+import { getUserBooks } from '../services'
+import BooksList from './BooksList'
+
+export default function Home({ userEmail }) {
+  const [isLoading, setIsLoading] = useState(true)
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    loadBooks()
+  }, [])
+
+  const loadBooks = async () => {
+    const response = await getUserBooks({ userEmail })
+    if (response !== undefined && response.status === 200) {
+      setBooks(response.data.books)
+      setIsLoading(false)
+    }
+  }
+
+  return (
+    <div>
+      <h1>Bookshelf</h1>
+      {
+        isLoading
+          ? 'Loading...'
+          : books.length > 0
+            ? < BooksList />
+            : <h2>You don't have any books.</h2>
+      }
+    // TODO: Add new book Modal
+    </div>
+  )
+}
