fd6b014741a68ac7eab198a7700ee5821b766d3c
Diego Pineda <pineda.diego798@gmail.com>
2020-09-27 13:19:04 -0500

    Pdf url in Book view

--- client/src/Book/components/BookViewModal.js
+++ client/src/Book/components/BookViewModal.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react'
 import Modal from 'react-modal'
 import AddQuoteModal from './AddQuoteModal'
-import QuotesModal from './QuotesModal'
+import QuotesListModal from './QuotesListModal'
 
 Modal.setAppElement('#root')
 
 export default function BookViewModal({ book, modalOpen, setModalOpen, loadBooks }) {
   const [modalAddQuoteOpen, setModalAddQuoteOpen] = useState(false)
   const [modalQuotesOpen, setModalQuotesOpen] = useState(false)
-  const { _id, name, author, quotes } = book
+  const { _id, name, author, quotes, pdfUrl } = book
 
   const closeModal = () => setModalOpen(false)
 
@@ -24,8 +24,11 @@ export default function BookViewModal({ book, modalOpen, setModalOpen, loadBooks
         <h3>{author}</h3>
         <button onClick={closeModal} className='modal__close-button'>X</button>
       </div>
+      <div className='book__pdf'>
+        PDF READER VIEW: {pdfUrl}
+      </div>
       <button onClick={() => setModalAddQuoteOpen(true)}>Add quote</button>
-      <QuotesModal
+      <QuotesListModal
         bookName={name}
         quotes={quotes}
         modalOpen={modalQuotesOpen}
--- client/src/Book/components/QuotesListModal.js
+++ client/src/Book/components/QuotesListModal.js
@@ -3,7 +3,7 @@ import Modal from 'react-modal'
 
 Modal.setAppElement('#root')
 
-export default function QuotesModal({ quotes, bookName, modalOpen, setModalOpen }) {
+export default function QuotesListModal({ quotes, bookName, modalOpen, setModalOpen }) {
   const closeModal = () => setModalOpen(false)
 
   return (
@@ -14,12 +14,12 @@ export default function QuotesModal({ quotes, bookName, modalOpen, setModalOpen 
       className='modal__content'
     >
       <div className='modal__header'>
-        <h1>"{bookName}" saved quotes</h1>
+        <h1>"{bookName}" quotes</h1>
         <button onClick={closeModal} className='modal__close-button'>X</button>
       </div>
       <div className='quotes'>
         {
-          quotes.map((quote, index) => <p key={index}>{quote}</p>)
+          quotes.map((quote, index) => <p key={index}>* {quote}</p>)
         }
       </div>
     </Modal>
