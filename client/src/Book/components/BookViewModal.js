2e46ebb546e83fd31b8e22af4b916aefc604136e
Diego Pineda <pineda.diego798@gmail.com>
2020-09-27 16:48:37 -0500

    Show pdf in modal

--- client/src/Book/components/BookViewModal.js
+++ client/src/Book/components/BookViewModal.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import Modal from 'react-modal'
+import { Document, Page } from 'react-pdf'
 import AddQuoteModal from './AddQuoteModal'
 import QuotesListModal from './QuotesListModal'
 
@@ -8,9 +9,22 @@ Modal.setAppElement('#root')
 export default function BookViewModal({ book, modalOpen, setModalOpen, loadBooks }) {
   const [modalAddQuoteOpen, setModalAddQuoteOpen] = useState(false)
   const [modalQuotesOpen, setModalQuotesOpen] = useState(false)
+  const [numPages, setNumPages] = useState(null)
+  const [pageNumber, setPageNumber] = useState(1)
   const { _id, name, author, quotes, pdfUrl } = book
 
   const closeModal = () => setModalOpen(false)
+
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages)
+    setPageNumber(1)
+  }
+
+  const changePage = (offset) => setPageNumber(prevPageNumber => prevPageNumber + offset)
+
+  const previousPage = () => changePage(-1)
+
+  const nextPage = () => changePage(1)
 
   return (
     <Modal
@@ -25,7 +39,34 @@ export default function BookViewModal({ book, modalOpen, setModalOpen, loadBooks
         <button onClick={closeModal} className='modal__close-button'>X</button>
       </div>
       <div className='book__pdf'>
-        PDF READER VIEW: {pdfUrl}
+        <Document
+          file={pdfUrl}
+          options={{ workerSrc: 'pdf.worker.js' }}
+          onLoadSuccess={onDocumentLoadSuccess}
+        >
+          <Page
+            pageNumber={pageNumber}
+          />
+        </Document>
+        <div className='book__controls'>
+          <p>
+            Page {pageNumber || (numPages ? 1 : '--')} of {numPages || '--'}
+          </p>
+          <button
+            type='button'
+            disabled={pageNumber <= 1}
+            onClick={previousPage}
+          >
+            Previous
+        </button>
+          <button
+            type='button'
+            disabled={pageNumber >= numPages}
+            onClick={nextPage}
+          >
+            Next
+        </button>
+        </div>
       </div>
       <button onClick={() => setModalAddQuoteOpen(true)}>Add quote</button>
       <QuotesListModal
